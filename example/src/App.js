ed0dd51f71e0b248d2e542cb8c1f612cea61c72a
RedLynx87 <115522437+RedLynx87@users.noreply.github.com>
2022-10-13 14:49:40 +0700

    typography

--- example/src/App.js
+++ example/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Button } from 'bigstyle'
+import { Button,Text } from 'bigstyle'
 import 'bigstyle/dist/index.css'
 
 const App = () => {
@@ -19,6 +19,21 @@ const App = () => {
       <Button type="outlined" disable={true}>Outline,disabled</Button>
       <Button type="secondary" textOnly={true} disable={true}>Secondary, Text Only,disabled</Button>
       <Button type="outlined" textOnly={true} disable={true}>Outline, Text Only,disabled</Button>
+    </div>
+    <div>
+      <h4>Typography</h4>
+      <Text type="label-1" textWeight="regular">Halo, selamat pagi!</Text>
+      <Text type="label-1" textWeight="medium">Halo, selamat pagi!</Text>
+      <Text type="label-1" textWeight="bold">Halo, selamat pagi!</Text>
+      <Text type="label-2" textWeight="regular">Halo, selamat pagi!</Text>
+      <Text type="label-2" textWeight="medium">Halo, selamat pagi!</Text>
+      <Text type="label-2" textWeight="bold">Halo, selamat pagi!</Text>
+      <Text type="label-3" textWeight="regular">Halo, selamat pagi!</Text>
+      <Text type="label-3" textWeight="medium">Halo, selamat pagi!</Text>
+      <Text type="label-3" textWeight="bold">Halo, selamat pagi!</Text>
+      <Text type="label-4" textWeight="regular">Halo, selamat pagi!</Text>
+      <Text type="label-4" textWeight="medium">Halo, selamat pagi!</Text>
+      <Text type="label-4" textWeight="bold">Halo, selamat pagi!</Text>
     </div>
     </>
   )
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './styles.module.css'
 
 export {default as Button} from './Button'
+export {default as Text} from './Text'
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
